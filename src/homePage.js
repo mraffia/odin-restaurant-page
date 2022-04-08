ebca45335a0945b365d51dd85a7fecf437e8d737
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-04-09 00:42:02 +0700

    Remove homePage.js unnecessary divs

--- src/homePage.js
+++ src/homePage.js
@@ -1,14 +1,10 @@
 export default function generateHomePage() {
     const p = document.createElement('p');
-    const container = document.createElement('div');
     const homePageContent = document.createElement('div');
-
-    container.setAttribute('id', 'container');
 
     p.textContent = "Welcome to Aliazone! A (self-proclaimed) 5 star restaurant with delicious food and drinks. Take a look at our menus! And don't hesitate to order or ask us anything via the numbers at the contact page";
 
-    container.appendChild(p);
-    homePageContent.appendChild(container);
+    homePageContent.appendChild(p);
 
     return homePageContent;
 }
