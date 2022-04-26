df6ca6d8bd27add19e8f019e461e9a1a1f3c4783
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-04-26 09:36:05 +0700

    Add page navigation tabs their stylings

--- src/homePage.js
+++ src/homePage.js
@@ -9,7 +9,7 @@ export default function generateHomePage() {
     p.classList.add('home-desc');
     homePageContent.classList.add('home-page');
 
-    p.textContent = "Welcome to Sly's Shop! Sly is a Merchant in Hollow Knight. He sells many wares at his shop in Dirtmouth for Geo but drives a hard bargain. Take a look at the menus! Don't hesitate to order or ask Sly anything via the numbers at the contact page";
+    p.textContent = "Welcome to Sly's Shop! Sly is a Merchant in Hollow Knight. He sells many wares at his shop in Dirtmouth for Geo but drives a hard bargain. Take a look at the goods! Don't hesitate to order or ask Sly anything via the numbers at the contact page.";
     img.src = SlyShop;
 
     homePageContent.appendChild(img);
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import generateMenuPage from './menuPage.js';
 function currentPage() {
     const header = document.createElement('div');
     const h1 = document.createElement('h1');
+    const tabs = document.createElement('ul');
     const content = document.createElement('div');
     const footer = document.createElement('div');
 
@@ -14,12 +15,36 @@ function currentPage() {
     footer.setAttribute('id', 'footer');
 
     h1.classList.add('page-header');
+    tabs.classList.add('tabs');
+
+    let pages = [
+        {
+            name: "Home",
+            pageContent: generateHomePage()
+        },
+        {
+            name: "Goods",
+            pageContent: generateMenuPage()
+        },
+        {
+            name: "Contact",
+            pageContent: generateContactPage()
+        },
+    ];
+
+    for (let i = 0; i < pages.length; i++) {
+        const tab = document.createElement('li');
+        tab.classList.add('tab');
+        tab.textContent = pages[i].name;
+        tabs.appendChild(tab);
+    }
 
     h1.textContent = "Great Nailsage Sly's Shop";
     footer.innerHTML = 'By yours truly, <a href="https://github.com/mraffia"> mraffia</a>';
 
     header.appendChild(h1);
-    content.appendChild(generateMenuPage());
+    header.appendChild(tabs);
+    content.appendChild(generateHomePage());
     
     document.body.appendChild(header);
     document.body.appendChild(content);
