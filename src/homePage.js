3619afbd18c70fcbb9c93ccaa0f8b69334f96107
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-04-26 07:40:02 +0700

    Fix homepage layout

--- src/homePage.js
+++ src/homePage.js
@@ -6,6 +6,7 @@ export default function generateHomePage() {
     const homePageContent = document.createElement('div');
 
     img.classList.add('banner');
+    homePageContent.classList.add('home-page');
 
     p.textContent = "Welcome to Aliazone! A (self-proclaimed) 5 star restaurant with delicious food and drinks. Take a look at our menus! And don't hesitate to order or ask us anything via the numbers at the contact page";
     // Photo by Igor Starkov: https://www.pexels.com/photo/photo-of-cafe-interior-1307698/
